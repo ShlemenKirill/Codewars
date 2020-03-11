ea9b904725f6c872ac96a896acdba23d1e9add1c
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-11 21:08:35 +0300

    Create project

---
+++ Sum of Minimums!/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sum_of_Minimums_
+{
+    class Program
+    {
+        public static int SumOfMinimums(int[,] numbers)
+        {
+            
+            
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
