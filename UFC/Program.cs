6ed48ec0ef117448319385c85189b30315d74651
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-14 23:30:31 +0300

    Solved

--- UFC/Program.cs
+++ UFC/Program.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace UFC
 {
-    class Program
+    class Kata
     {
         public static string Quote(string fighter)
         {
-            string McGregor = "Conor McGregor";
-            string McGregor_speech = "I'd like to take this chance to apologize.. To absolutely NOBODY!";
-            string SaintPierre = "george saint pierre";
+            string figter_lower = fighter.ToLower();
+            string McGregor = "conor mcgregor";
+            string McGregor_speech = "I'd like to take this chance to apologize.. To absolutely NOBODY!";            
             string SaintPierre_speech = "I am not impressed by your performance.";
-            if (fighter == McGregor)
+            if (figter_lower == McGregor)
             {
                 return McGregor_speech;
             }
@@ -18,7 +19,10 @@ namespace UFC
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Assert.AreEqual("I am not impressed by your performance.", Kata.Quote("george saint pierre"));
+            Assert.AreEqual("I'd like to take this chance to apologize.. To absolutely NOBODY!", Kata.Quote("conor mcgregor"));
+            Assert.AreEqual("I am not impressed by your performance.", Kata.Quote("George Saint Pierre"));
+            Assert.AreEqual("I'd like to take this chance to apologize.. To absolutely NOBODY!", Kata.Quote("Conor McGregor"));
         }
     }
 }
