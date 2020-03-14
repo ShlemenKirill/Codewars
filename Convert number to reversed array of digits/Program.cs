a60b20f32ece0f15ae71ac49d1471c96cddb2419
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-15 00:29:17 +0300

    Solved

--- Convert number to reversed array of digits/Program.cs
+++ Convert number to reversed array of digits/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Convert_number_to_reversed_array_of_digits
 {
-    class Program
+    class Digitizer
     {
+        public static long[] Digitize(long n)
+        {
+
+            List<long> digits = new List<long>();
+            do
+            {
+                digits.Add(Math.Abs(n % 10));
+            } while ((n /= 10) != 0);
+            return digits.ToArray();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Assert.AreEqual(new long[] { 1, 3, 2, 5, 3 }, Digitizer.Digitize(35231));
         }
     }
 }
