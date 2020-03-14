1f2693a1b88c4c67c345a8c4519fe4925ef89775
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-14 19:44:44 +0300

    Add project

---
+++ UFC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UFC
+{
+    class Program
+    {
+        public static string Quote(string fighter)
+        {
+            string McGregor = "Conor McGregor";
+            string McGregor_speech = "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+            string SaintPierre = "george saint pierre";
+            string SaintPierre_speech = "I am not impressed by your performance.";
+            if (fighter == McGregor)
+            {
+                return McGregor_speech;
+            }
+            return SaintPierre_speech;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
