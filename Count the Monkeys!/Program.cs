48ada11f0a872099766ae0eb9216a10c3b3b9bf1
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-15 00:35:23 +0300

    Solved

---
+++ Count the Monkeys!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Count_the_Monkeys_
+{
+    class Program
+    {
+        public static int[] MonkeyCount(int n)
+        {
+
+            int[] mas = new int[n];
+            for (int i = 0; i < n ; i++)
+            {
+                mas[i] = i+1;
+
+            }
+            return mas;
+        }
+        static void Main(string[] args)
+        {
+            MonkeyCount(5);
+        }
+    }
+}
