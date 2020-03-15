a23a41382562aae8bac04ab20935f93aa4264b1b
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-15 23:56:28 +0300

    Create project

---
+++ List Filtering/Program.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace List_Filtering
+{
+    class ListFilterer
+    {
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            Regex regex = new Regex(@"\d");
+            string a = listOfItems.ToString;
+            IEnumerable<int> evens = from i in listOfItems
+                                     where i => regex
+                                     select i;
+            return evens;
+        }
+        static void Main(string[] args)
+        {
+            var list = new List<object>() { 1, 2, "a", "b" };
+            var expected = new List<int>() { 1, 2 };
+            var actual = ListFilterer.GetIntegersFromList(list);
+            Assert.IsTrue(expected.SequenceEqual(actual));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
