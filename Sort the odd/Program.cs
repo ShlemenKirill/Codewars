446854c15623aef874cb09de13fcadf8264d5519
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-16 23:28:49 +0300

    Solved

--- Sort the odd/Program.cs
+++ Sort the odd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Sort_the_odd
 {
@@ -7,7 +8,30 @@ namespace Sort_the_odd
     {
         public static int[] SortArray(int[] array)
         {
-            return array;
+            List<int> odd = new List<int>(array.Length);
+            List<int> index = new List<int>(array.Length);
+            int[] mas = new int[array.Length];            
+            for (int i = 0; i < array.Length; i++)
+            {                
+                if (array[i]%2 == 1)
+                {
+                    odd.Add(array[i]);
+                    index.Add(i);
+                }
+                else
+                {
+                    mas[i] = array[i];
+                }                
+            }
+            odd.Sort();            
+            
+            for (int j = 0; j < odd.Count; j++)
+            {
+                mas[index[j]] = odd[j];
+                
+            }
+                        
+            return mas;
         }
         static void Main(string[] args)
         {
