d09d9349c8b4a9666f84cb960e1e1e073742c1b7
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-27 20:24:42 +0300

    Solved

---
+++ Number of Decimal Digits/Program.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Number_of_Decimal_Digits
+{
+    public class DecTools
+    {
+        public static int Digits(ulong n)
+        {
+            string s = n.ToString();
+            int res = s.Length;
+            return res;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assert.AreEqual(1, DecTools.Digits(5ul));
+            Assert.AreEqual(5, DecTools.Digits(12345ul));
+            Assert.AreEqual(10, DecTools.Digits(9876543210ul));
+        }
+    }
+}
