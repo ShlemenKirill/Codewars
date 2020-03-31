432413ef7a72aead3280016817fd51054a686281
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-31 22:28:26 +0300

    Solved

--- Duplicate Encoder/Program.cs
+++ Duplicate Encoder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Duplicate_Encoder
 {
@@ -7,7 +8,36 @@ namespace Duplicate_Encoder
     {
         public static string DuplicateEncode(string word)
         {
-            return word;
+            string lower =  word.ToLower();
+            char[] a = lower.ToCharArray();
+            List<char> str = new List<char>();
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[i].Equals(a[j]))
+                    {                                              
+                        count++;                        
+                    }                   
+                    
+                }
+                if (count > 1)
+                {
+                    str.Add(')');
+                    
+                }
+
+                if (count < 2)
+                {
+                    str.Add('(');
+                    
+                }
+                count = 0;
+            }
+            
+            var result = String.Join("", str.ToArray());
+            return result;
         }
     }
     class Program
