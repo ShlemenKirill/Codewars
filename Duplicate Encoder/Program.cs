8cd61ed212d413f98c2d32d3c841f207435f5218
Шлемен Кирилл <shlemen_kirill@outlook.com>
2020-03-31 20:36:05 +0300

    Create

---
+++ Duplicate Encoder/Program.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Duplicate_Encoder
+{
+    public class Kata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            return word;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assert.AreEqual("(((", Kata.DuplicateEncode("din"));
+            Assert.AreEqual("()()()", Kata.DuplicateEncode("recede"));
+            Assert.AreEqual(")())())", Kata.DuplicateEncode("Success"), "should ignore case");
+            Assert.AreEqual("))((", Kata.DuplicateEncode("(( @"));
+        }
+    }
+}
